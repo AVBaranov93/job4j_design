51709b1e14dec8619cf8a7c0f5aedf41a1409b89
AVBaranov93 <avbaranov@internet.ru>
2023-12-28 23:16:19 +1000

    6. Читаем файл конфигурации [#858]

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -23,8 +23,26 @@ class ConfigTest {
     }
 
     @Test
-    void whenPairWithViolation() {
-        String path = "data/pair_with_violation.properties";
+    void whenPairWithoutValue() {
+        String path = "data/pair_without_value.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("illegal argument");
+    }
+
+    @Test
+    void whenPairWithoutKey() {
+        String path = "data/pair_without_key.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("illegal argument");
+    }
+
+    @Test
+    void whenPairWithoutKeyAndValue() {
+        String path = "data/pair_without_key_and_value.properties";
         Config config = new Config(path);
         assertThatThrownBy(config::load)
                 .isInstanceOf(IllegalArgumentException.class)
