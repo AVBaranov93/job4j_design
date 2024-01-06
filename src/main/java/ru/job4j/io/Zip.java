f49a8a5a02a270d11b85520c3ccbd92c87dbaa74
AVBaranov93 <avbaranov@internet.ru>
2024-01-06 23:43:01 +1000

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,21 +3,19 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public void packFiles(String[] args) {
-        validateInput(args);
-        ArgsName argsName = ArgsName.of(args);
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(argsName.get("o"))))) {
-            for (Path inner : Search.search(Path.of(argsName.get("d")), e -> !e.toString().endsWith(argsName.get("e")))) {
-                zip.putNextEntry(new ZipEntry(inner.toAbsolutePath().toString()));
-                try (BufferedInputStream output = new BufferedInputStream(new FileInputStream(inner.toFile()))) {
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toAbsolutePath().toString()));
+                try (BufferedInputStream output = new BufferedInputStream(new FileInputStream(source.toFile()))) {
                     zip.write(output.readAllBytes());
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,7 +32,7 @@ public class Zip {
         }
     }
 
-    private void validateInput(String[] args) {
+    private static void validateInput(String[] args) {
         if (args.length != 3) {
             throw new IllegalArgumentException("Arguments not passed to program");
         }
@@ -50,13 +48,14 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
         zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("C:/Users/ashot/Desktop/pom.zip")
         );
-        zip.packFiles(args);
-
+        validateInput(args);
+        ArgsName argsName = ArgsName.of(args);
+        zip.packFiles(Search.search(Path.of(argsName.get("d")), e -> !e.toString().endsWith(argsName.get("e"))), new File(argsName.get("o")));
     }
 }
