ed065df530cb66205d49746432e3fced535c954a
andrey <avbaranov@internet.ru>
2024-02-07 08:44:36 +1000

    2. Схема прав пользователи и роли. [#1093 #479323]

--- db/insert.sql
+++ db/insert.sql
@@ -6,4 +6,4 @@ insert into categories (category) values ('test category');
 insert into states (state) values ('test state');
 insert into items (item, user_id, category_id, state_id) values ('add item', 1, 1, 1);
 insert into comments (comment, item_id) values ('test comment', 1);
-insert into attaches (attached_file, item_id) values ('test file', 1);
+insert into attachs (attached_file, item_id) values ('test file', 1);
