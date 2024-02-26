4e381ccd506d79e8f17984c670304904518fa3d2
AVBaranov93 <avbaranov@internet.ru>
2024-02-26 23:05:21 +1000

    10. Операторы множества в SQL [#504896]

---
+++ db/union_queries/union_queries.sql
@@ -0,0 +1,11 @@
+select name from movie intersect
+select title from book;
+
+select title from book except
+select name from movie;
+
+(select name from movie except
+select title from book)
+union all
+(select title from book except
+select name from movie);
